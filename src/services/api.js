3810d146daca9dae1cb8ccc6bdb058e7364c8f2a
mustafacapras <mustafaacapras@gmail.com>
2025-05-09 00:33:27 +0200

    fixed2

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://todoapp-backend.onrender.com/api';
+//const API_URL = 'https://todoapp-backend.onrender.com/api';
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
